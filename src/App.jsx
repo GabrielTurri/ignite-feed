b5b043fee7053090d99b8ba11c88a6e93594da55
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-08-29 10:15:57 -0300

    sidebar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,21 @@
 import { Post } from './Post'
 import { Header } from './Components/Header'
+import { Sidebar } from './Components/Sidebar'
 
+import styles from './App.module.css'
 import './global.css'
 
 function App() {
   return (
     <>
     <Header/>
-    <Post/>
-    <Post/>
-    <Post/>
+
+    <div className={styles.wrapper}>
+      <Sidebar />
+      <main>
+        <Post></Post>
+      </main>
+    </div>
     </>
   )
 }
