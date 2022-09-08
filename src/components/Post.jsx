49fa4d850d0d5b720bf8c9b62795dcce89039a73
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-09-08 11:09:14 -0300

    creating Avatar component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,13 @@
 import styles from './Post.module.css'
 import { Comment } from './Comment'
+import { Avatar } from './Avatar'
 
 export function Post() {
   return(
     <article className={styles.post}>
       <header>
         <div className={styles.author}>
-          <img className={styles.avatar} src="https://github.com/gabrielturri.png"/>
+          <Avatar hasBorder src="https://github.com/gabrielturri.png"/>
           <div className={styles.authorInfo}>
             <strong>Gabriel Turri</strong>
             <span>Web Developer</span>
