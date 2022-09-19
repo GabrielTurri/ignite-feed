44cb4503add450eeb262b9fd7832e43373f7368e
Gabriel Turri <ga-briel.t.a@hotmail.com>
2022-09-19 11:39:07 -0300

    post props and map

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,42 @@ import { Header } from './components/Header'
 import { Sidebar } from './components/Sidebar'
 
 import styles from './App.module.css'
+
 import './global.css'
+
+// author: {avatar_url:"", name: "", role: ""} 
+// publishedAt: Date
+// content: String
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/gabrielturri.png',
+      name: 'Gabriel Turri',
+      role: 'Front End Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!', },
+      { type: 'paragraph', content: 'Acabei de dar um exemplo', },
+      { type: 'paragraph', content: '<a href="https://github.com/gabrielturri">Repositório no Github</a>', },
+    ],
+    publishedAt: new Date('2022-09-15 12:00:00')
+  }, {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/lucasadum.png',
+      name: 'Lucas Adum',
+      role: 'Back End Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galera!', },
+      { type: 'paragraph', content: 'Acabei de dar outro exemplo', },
+      { type: 'paragraph', content: '<a href="https://github.com/gabrielturri">Repositório no Github</a>', },
+    ],
+    publishedAt: new Date('2022-09-17 12:00:00')
+  },
+];
 
 function App() {
   return (
@@ -13,7 +48,16 @@ function App() {
     <div className={styles.wrapper}>
       <Sidebar />
       <main>
-        <Post />
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        }
+        )}
       </main>
     </div>
     </>
